e6ed9e175b1d96466ede2e81f5d7537842648be4
lunar-mycroft <timothy.l.park+github@gmail.com>
2016-11-30 22:53:43 -0500

    Refined Layout
    All buttons should be at least present now.

--- BulkFret.m
+++ BulkFret.m
@@ -1,9 +1,24 @@
-mainWindow = figure('Visible','on','Position',[100,100,1375,900],'Resize','off');
+mainWindow = figure('Visible','on','Position',[25,25,1450,950],'Resize','off');
 
-cells=[];
+cells={}
+rowBacks=[]
 
 for i=1:12
     for j=1:8
-         miniCell.create(110*i-85,110*j-85,100,100,1:10,mainWindow);
+         miniCell.create(110*i,110*j-85,100,100,1:10,mainWindow);
     end
 end
+
+for i=1:8
+    uicontrol('Style','popupmenu',...
+           'String',{'--','A1','A2','A3','A4','A5','A6','A7','A8','A9','A10','A11','A12','B1','B2','B3','B4','B5','B6','B7','B8','B9','B10','B11','B12','C1','C2','C3','C4','C5','C6','C7','C8','C9','C10','C11','C12','D1','D2','D3','D4','D5','D6','D7','D8','D9','D10','D11','D12','E1','E2','E3','E4','E5','E6','E7','E8','E9','E10','E11','E12','F1','F2','F3','F4','F5','F6','F7','F8','F9','F10','F11','F12','G1','G2','G3','G4','G5','G6','G7','G8','G9','G10','G11','G12','H1','H2','H3','H4','H5','H6','H7','H8','H9','H10','H11','H12'}, 'Position', [10, 110*i-50,100,15]);
+end
+
+for i=1:12
+    uicontrol('Style','popupmenu',...
+           'String',{'--','A1','A2','A3','A4','A5','A6','A7','A8','A9','A10','A11','A12','B1','B2','B3','B4','B5','B6','B7','B8','B9','B10','B11','B12','C1','C2','C3','C4','C5','C6','C7','C8','C9','C10','C11','C12','D1','D2','D3','D4','D5','D6','D7','D8','D9','D10','D11','D12','E1','E2','E3','E4','E5','E6','E7','E8','E9','E10','E11','E12','F1','F2','F3','F4','F5','F6','F7','F8','F9','F10','F11','F12','G1','G2','G3','G4','G5','G6','G7','G8','G9','G10','G11','G12','H1','H2','H3','H4','H5','H6','H7','H8','H9','H10','H11','H12'}, 'Position', [110*i-0, 910,100,15]);
+end
+
+showNorms=uicontrol('Style','pushbutton',...
+           'String','Show Normalize Plots',...
+           'Position',[675,925,150,25]);
