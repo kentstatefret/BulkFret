827dafeacfad2f6927c7195baf2060bd57cc91dc
lunar-mycroft <timothy.l.park+github@gmail.com>
2016-12-07 16:00:45 -0500

    Layout take 2
    Redid the layout from scratch.  Many of the inputs are non functioning,
    still don't have a way to open the file.

--- BulkFret.m
+++ BulkFret.m
@@ -1,24 +1,91 @@
-mainWindow = figure('Visible','on','Position',[25,25,1450,950],'Resize','off');
+function bulkFret
+    clc
 
-cells={}
-rowBacks=[]
+    fid=fopen('example.xls');
+    line = fgetl(fid);
+    lines = [];
 
-for i=1:12
-    for j=1:8
-         miniCell.create(110*i,110*j-85,100,100,1:10,mainWindow);
+    while ischar(line)
+        lines{end+1,1} = line;
+        line = fgetl(fid);
+    end
+    fclose(fid);
+    cells=[];
+
+    for i=7:size(lines,1)-12
+        cLine=strsplit(char(lines(i)));
+        if size(cLine,2)==99
+            cells=[cells;cLine];
+        end
+    end
+
+    doubles=zeros(size(cells));
+
+    for i=1:size(cells,1)
+        for j=1:size(cells,2)
+            if size(char(cells(i,j)),2)~=1
+                raw=char(cells(i,j));
+                striped=[];
+                for k=1:size(raw,2)
+                    if rem(k,2)==0
+                        striped=[striped,raw(k)];
+                    end
+                end
+                doubles(i,j)=str2double(striped);
+            end
+        end
+    end
+
+    backgroundsStrings={'  '};
+    backgroundIDs=[];
+    selectedCells=[];
+    cellTable={'A1','A2','A3','A4','A5','A6','A7','A8','A9','A10','A11','A12';'B1','B2','B3','B4','B5','B6','B7','B8','B9','B10','B11','B12';'C1','C2','C3','C4','C5','C6','C7','C8','C9','C10','C11','C12';'D1','D2','D3','D4','D5','D6','D7','D8','D9','D10','D11','D12';'E1','E2','E3','E4','E5','E6','E7','E8','E9','E10','E11','E12';'F1','F2','F3','F4','F5','F6','F7','F8','F9','F10','F11','F12';'G1','G2','G3','G4','G5','G6','G7','G8','G9','G10','G11','G12';'H1','H2','H3','H4','H5','H6','H7','H8','H9','H10','H11','H12'};
+    cellList={'A1','A2','A3','A4','A5','A6','A7','A8','A9','A10','A11','A12','B1','B2','B3','B4','B5','B6','B7','B8','B9','B10','B11','B12','C1','C2','C3','C4','C5','C6','C7','C8','C9','C10','C11','C12','D1','D2','D3','D4','D5','D6','D7','D8','D9','D10','D11','D12','E1','E2','E3','E4','E5','E6','E7','E8','E9','E10','E11','E12','F1','F2','F3','F4','F5','F6','F7','F8','F9','F10','F11','F12','G1','G2','G3','G4','G5','G6','G7','G8','G9','G10','G11','G12','H1','H2','H3','H4','H5','H6','H7','H8','H9','H10','H11','H12'};
+    
+    mainWindow          = figure('Visible','on','Position',[25,25,1280,720],'Resize','off');
+    table               = uitable(mainWindow,'Data',cellTable,'Position',[10,530,332,168],'ColumnWidth',{25,25,25,25,25,25,25,25,25,25,25,25},'CellSelectionCallback',@CellSelection);
+    graph               = axes('Units','Pixels','Position',[400,100,800,600],'Parent',mainWindow);
+    addToBackgrounds	= uicontrol('Style','pushbutton','String','Add selected cells to background list.','Position',[10,500,200,25],'Callback',@addBackgrounds);
+    useBackgrounds      = uicontrol('Style','popupmenu','String',backgroundsStrings,'Callback',@useBackground,'Position',[200,450,50,25]);
+    backgroundLable     = uicontrol('Style','text','String','Select Background for current cells','HorizontalAlignment','left','Position',[10,457,175,15]);
+    backgroundEnable	= uicontrol('Style','checkbox','String','Subtract Backgrounds','Position',[10,425,150,25]);
+    normEnable          = uicontrol('Style','checkbox','String','Normalize plots','Position',[10,400,125,25]);
+    
+    
+    backgrounds=zeros(size(doubles,2),1);
+    normalizations=ones(size(doubles,2),1)*.5;
+   
+    function CellSelection(source,event)
+        data=doubles;
+        tempList=event.Indices;
+        selectedCells=zeros(size(tempList,1),1);
+        for i=1:size(selectedCells,1)
+            selectedCells(i)=12*(tempList(i,1)-1)+(tempList(i,2)-1);
+        end
+        X=data(1:end,1);
+        
+        plot(graph,[600,600],[0,0])
+        axis([min(X),max(X),0,max(max(data(1:end,3:end)))])
+        for i=1:size(selectedCells,1)
+            selectedCells(i);
+            Y=data(1:end,selectedCells(i)+3);
+            if sum(Y)~=0
+                hold on
+                plot(graph,X,Y)
+                axis([min(X),max(X),0,max(max(data(1:end,3:end)))])
+                hold off
+            end  
+        end
+    end
+    function addBackgrounds(source,event)
+        backgroundIDs=[backgroundIDs;selectedCells];
+        backgroundsStrings={'--'};
+        for i=1:size(backgroundIDs,1)
+            backgroundStrings(i+1)=cellList(backgroundIDs(i)+1);
+        end
+        set(useBackgrounds,'String',backgroundStrings);
+    end
+    function useBackground(source,event)
+        backgroundID=backgroundIDs(get(source,'Value')-1)
     end
 end
-
-for i=1:8
-    uicontrol('Style','popupmenu',...
-           'String',{'--','A1','A2','A3','A4','A5','A6','A7','A8','A9','A10','A11','A12','B1','B2','B3','B4','B5','B6','B7','B8','B9','B10','B11','B12','C1','C2','C3','C4','C5','C6','C7','C8','C9','C10','C11','C12','D1','D2','D3','D4','D5','D6','D7','D8','D9','D10','D11','D12','E1','E2','E3','E4','E5','E6','E7','E8','E9','E10','E11','E12','F1','F2','F3','F4','F5','F6','F7','F8','F9','F10','F11','F12','G1','G2','G3','G4','G5','G6','G7','G8','G9','G10','G11','G12','H1','H2','H3','H4','H5','H6','H7','H8','H9','H10','H11','H12'}, 'Position', [10, 110*i-50,100,15]);
-end
-
-for i=1:12
-    uicontrol('Style','popupmenu',...
-           'String',{'--','A1','A2','A3','A4','A5','A6','A7','A8','A9','A10','A11','A12','B1','B2','B3','B4','B5','B6','B7','B8','B9','B10','B11','B12','C1','C2','C3','C4','C5','C6','C7','C8','C9','C10','C11','C12','D1','D2','D3','D4','D5','D6','D7','D8','D9','D10','D11','D12','E1','E2','E3','E4','E5','E6','E7','E8','E9','E10','E11','E12','F1','F2','F3','F4','F5','F6','F7','F8','F9','F10','F11','F12','G1','G2','G3','G4','G5','G6','G7','G8','G9','G10','G11','G12','H1','H2','H3','H4','H5','H6','H7','H8','H9','H10','H11','H12'}, 'Position', [110*i-0, 910,100,15]);
-end
-
-showNorms=uicontrol('Style','pushbutton',...
-           'String','Show Normalize Plots',...
-           'Position',[675,925,150,25]);
