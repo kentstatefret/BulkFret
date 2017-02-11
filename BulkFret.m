2bd56a13294b371f352548fdee5652407f2883fb
lunar-mycroft <timothy.l.park+github@gmail.com>
2017-02-10 22:04:46 -0500

    Updated file opening
    It (finally!) allows the opening of arbitrary files.  Note: I don't know
    if it works for _all_ files from this machine.  More testing needed.

--- BulkFret.m
+++ BulkFret.m
@@ -1,41 +1,7 @@
 function bulkFret
     clc
-
-    fid=fopen('example.xls');
-    line = fgetl(fid);
-    lines = [];
-
-    while ischar(line)
-        lines{end+1,1} = line;
-        line = fgetl(fid);
-    end
-    fclose(fid);
-    cells=[];
-
-    for i=7:size(lines,1)-12
-        cLine=strsplit(char(lines(i)));
-        if size(cLine,2)==99
-            cells=[cells;cLine];
-        end
-    end
-
-    doubles=zeros(size(cells));
-
-    for i=1:size(cells,1)
-        for j=1:size(cells,2)
-            if size(char(cells(i,j)),2)~=1
-                raw=char(cells(i,j));
-                striped=[];
-                for k=1:size(raw,2)
-                    if rem(k,2)==0
-                        striped=[striped,raw(k)];
-                    end
-                end
-                doubles(i,j)=str2double(striped);
-            end
-        end
-    end
-
+    doubles=zeros(116,99);
+    
     backgroundsStrings={'  '};
     backgroundIDs=[];
     selectedCells=[];
@@ -45,7 +11,8 @@ function bulkFret
     mainWindow          = figure('Name','Bulk Fret','NumberTitle','off','Visible','on','Position',[25,25,1280,720],'ResizeFcn',@resizeWindow);
     mainWindow.MenuBar='none';
     fileMenu=uimenu('Label','File');
-    file=[];
+    file='';
+    path='';
     uimenu(fileMenu,'Label','Open','Callback',@openFile);
     uimenu(fileMenu,'Label','Save','Callback','disp(''save'')');
     uimenu(fileMenu,'Label','Quit','Callback','disp(''exit'')');
@@ -65,6 +32,38 @@ function bulkFret
     normalize=false;
     function openFile(source,event)
         [file,path]=uigetfile({'*.m';'*.slx';'*.mat';'*.*'},'File Selector')
+        fid=fopen(strcat(path,file));
+        line = fgetl(fid);
+        lines = [];
+
+        while ischar(line)
+            lines{end+1,1} = line;
+            line = fgetl(fid);
+        end
+        fclose(fid);
+        cells=[];
+
+        for i=7:size(lines,1)-12
+            cLine=strsplit(char(lines(i)));
+            if size(cLine,2)==99
+                cells=[cells;cLine];
+            end
+        end
+
+        for i=1:size(cells,1)
+            for j=1:size(cells,2)
+                if size(char(cells(i,j)),2)~=1
+                    raw=char(cells(i,j));
+                    striped=[];
+                    for k=1:size(raw,2)
+                        if rem(k,2)==0
+                            striped=[striped,raw(k)];
+                        end
+                    end
+                    doubles(i,j)=str2double(striped);
+                end
+            end
+        end
     end
     function resizeWindow(source,event)
         newPos  = get(mainWindow,'Position');
